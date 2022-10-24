26dd8e3ef9bac9abf73511bf8e7a36a29a12d9f6
visalsrimanga <visalsrimanga@gmail.com>
2022-10-24 08:30:23 +0530

    change the background image

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 const player = document.getElementById('player');
 const ground = document.getElementById('ground');
+const crows = document.getElementById('crows');
 let dx = 0;
 let dy = 0;
 let acceleration = 0.1;
 let index = 1;
+let crowsX = 0;
 
 const animate = ()=>{
 
     /* Animation part of the image */
     if(dx !== 0){
         player.style.backgroundImage = `url('img/Run\ \(${index++}\).png')`;
-    } else if(dy > 0.5 || dy < 0){
+    } else if(dy > 0.6 || dy < 0){
         player.style.backgroundImage = `url('img/Jump\ \(${index++}\).png')`;
     } else{
         player.style.backgroundImage = `url('img/Idle\ \(${index++}\).png')`;
@@ -38,6 +40,10 @@ const animate = ()=>{
 
     player.style.left= `${player.offsetLeft+dx}px`;
     player.style.top= `${player.offsetTop+dy}px`;
+
+
+    /* crows flying animations */
+
 
     requestAnimationFrame(animate);
 }
